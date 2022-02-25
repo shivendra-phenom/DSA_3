89dd8193d727ea124bac467da1c96d4ca015923d
shivendra-phenom <99706170+shivendra-phenom@users.noreply.github.com>
2022-02-25 23:14:20 +0530

    Add files via upload

---
+++ src/MaxMinInUnsortedArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MaxMinInUnsortedArray {
+    static int[] solution(int[] arr){
+        int[] ans = new int[2];
+        ans[0] = arr[0];
+        ans[1] = arr[0];
+        for(int k : arr){
+            if(k > ans[0])
+                ans[0] = k;
+            if(k < arr[1])
+                ans[1] = k;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        System.out.print("Enter the no of element in array: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++)
+            arr[i] = sc.nextInt();
+        System.out.print("Max = " + solution(arr)[0] + " Min = " + solution(arr)[1]);
+
+    }
+}
